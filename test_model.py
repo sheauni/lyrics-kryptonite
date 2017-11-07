e88afe76a859563dc97fa74f92826b7e0740d671
Nini <d10531318@gapps.fg.tp.edu.tw>
2017-11-07 23:02:47 +0800

    20171107

--- test_model.py
+++ test_model.py
@@ -1,59 +1,22 @@
-# coding:utf-8
+# -*- coding: utf-8 -*-
 
-import sys
 import gensim
-import sklearn
-import numpy as np
 
-from gensim.models.doc2vec import Doc2Vec, LabeledSentence
+#loading the model
+model = gensim.models.doc2vec.Doc2Vec.load('/Users/Nini/Desktop/schoolproject/LyricsFile22124/one_thousand.vec')
+#start testing
+#printing the vector of document at index 1 in docLabels
+f=open('/Users/Nini/Desktop/schoolproject/LyricsFile22124/s2.txt','r').readlines()
+i=0
+for line in f:
+    i+=1
+    if i==1011:
+        break
+    elif i>1000:
+        www = open('/Users/Nini/Desktop/schoolproject/LyricsFile22124/10songs/' + str(i) + '.txt', 'w')
+        www.write(line)
+        www.close()
+        newvec = model.infer_vector(['/Users/Nini/Desktop/schoolproject/LyricsFile22124/10songs/' + str(i) + '.txt'])
+        print newvec
+        print '\n\n'
 
-TaggededDocument = gensim.models.doc2vec.TaggedDocument
-
-def get_datasest():
-    with open("out/wangyi_title_cut.txt", 'r') as cf:
-        docs = cf.readlines()
-        print len(docs)
-
-    x_train = []
-    #y = np.concatenate(np.ones(len(docs)))
-    for i, text in enumerate(docs):
-        word_list = text.split(' ')
-        l = len(word_list)
-        word_list[l-1] = word_list[l-1].strip()
-        document = TaggededDocument(word_list, tags=[i])
-        x_train.append(document)
-
-    return x_train
-
-def getVecs(model, corpus, size):
-    vecs = [np.array(model.docvecs[z.tags[0]].reshape(1, size)) for z in corpus]
-    return np.concatenate(vecs)
-
-def train(x_train, size=200, epoch_num=1):
-    model_dm = Doc2Vec(x_train,min_count=1, window = 3, size = size, sample=1e-3, negative=5, workers=4)
-    model_dm.train(x_train, total_examples=model_dm.corpus_count, epochs=70)
-    model_dm.save('model/model_dm_wangyi')
-
-    return model_dm
-
-def test():
-    model_dm = Doc2Vec.load("model/model_dm_wangyi")
-    test_text = ['《', '舞林', '争霸' '》', '十强' '出炉', '复活', '舞者', '澳门', '踢馆']
-    inferred_vector_dm = model_dm.infer_vector(test_text)
-    print inferred_vector_dm
-    sims = model_dm.docvecs.most_similar([inferred_vector_dm], topn=10)
-
-
-    return sims
-
-if __name__ == '__main__':
-    x_train = get_datasest()
-    model_dm = train(x_train)
-
-    sims = test()
-    for count, sim in sims:
-        sentence = x_train[count]
-        words = ''
-        for word in sentence[0]:
-            words = words + word + ' '
-        print words, sim, len(sentence[0])
