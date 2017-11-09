55955f0cca09dca575ab1d171c99f155909ab037
Nini <d10531318@gapps.fg.tp.edu.tw>
2017-11-09 15:53:01 +0800

    2017.11.09

---
+++ try.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+import gensim
+import re
+import jieba
+import numpy
+#import array
+from sklearn.preprocessing import normalize
+
+
+punc = "ㄅㄆㄇㄈㄉㄊㄋㄌㄍㄎㄏˇˋㄐㄑㄒㄓㄔㄕㄖˊㄗㄘㄙ˙ㄧㄨㄩㄚㄛㄜㄝㄞㄟㄠㄡㄢㄣㄤㄥㄦ\￣︶▽ρ┬σ㊣．€↑↓↘↖↗↙→┴└┌♡《□■╬﹕。┘╭╮─▃▄▅▆▇█▉▊\╩╔╥◢◣●○οO◆◇﹉☆★〉〈﹒°∴◎⊙※║══１２３４５６７８９０ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＱＷＥＲＴＹＵＩＯＰＬＫＪＨＧＦＤＳＡＺＸＣＶＢＮＭ0123456789！？｡＂＃＄％＆＇（）＊＋，－／：；＜＝＞＠［＼］＾＿｀｛｜｝～｟｠｢｣､、〃》「」『』【】〔〕〖〗〘〙〚〛〜〝〞〟〰〾〿–—‘’‛“”„‟…‧﹏.!!#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"
+punc = punc.decode("utf-8")
+
+jieba.set_dictionary('/Library/Python/2.6/site-packages/jieba/dict.txt.big.txt')
+
+d2v_model = gensim.models.doc2vec.Doc2Vec.load('/Users/Nini/Desktop/schoolproject/LyricsFile22124/one_thousand.vec')
+biggest=0
+song=0
+
+#d2v=d2v_model.docvecs
+d2v=normalize(numpy.array(d2v_model.docvecs),norm='max',axis=1,copy=True, return_norm=False)
+www=open('/Users/Nini/Desktop/schoolproject/LyricsFile22124/say.txt','w')
+line=raw_input('輸入一句話：')
+www.write('<p> ')
+line = re.sub(ur"[%s]+" % punc, "", line.decode("utf-8"))
+print line
+line = line.strip(' ')
+if re.search('[a-zA-z]', line) is not None:
+    line = jieba.cut(line)
+    line = filter(lambda x: not x.isspace(), line)
+    www.write(('<l> <s> ' + u" ".join(line) + " </s> <\l> ").encode('utf-8'))
+else:
+    www.write('<l> ')
+            # print line
+            # line = line.decode('utf-8')
+    for sentence in line.split():
+                # www.write(word)
+        st = u" ".join(jieba.cut(sentence))
+                # print str
+        www.write(('<s> ' + st + " </s> ").encode('utf-8'))
+    www.write('</l> ')
+www.close()
+
+
+inputvec = d2v_model.infer_vector(['/Users/Nini/Desktop/schoolproject/LyricsFile22124/say.txt'])
+
+for i in range(0,1000):
+    docvec = d2v[i]
+    inner=0
+    for j in range(0,100):
+        k=inputvec[j]*docvec[j]
+        inner+=k
+    if inner>biggest:
+        biggest=inner
+        song=i+1
+print str(biggest)+'\n'
+print str(song)+'\n'
+mostsimilar=open('/Users/Nini/Desktop/schoolproject/LyricsFile22124/LyricsFile/'+str(song)+'.txt','r').readlines()
+for line in mostsimilar:
+    print line
+print d2v_model.docvecs[song-1]
+
